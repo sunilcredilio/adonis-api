a66ee680b458609ce257fae1e01b527bef0602d7
sunil samal <sunil.kumar.23.2@mountblue.tech>
2023-05-11 11:09:27 +0530

    Added hashing for password before save to the database

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,6 +1,7 @@
 import { DateTime } from "luxon";
-import { BaseModel, HasOne, column, hasOne } from "@ioc:Adonis/Lucid/Orm";
+import { BaseModel, HasOne, beforeSave, column, hasOne } from "@ioc:Adonis/Lucid/Orm";
 import Profile from "./Profile";
+import Hash from "@ioc:Adonis/Core/Hash";
 
 export default class User extends BaseModel {
   @column({ columnName: "id", isPrimary: true })
@@ -20,4 +21,11 @@ export default class User extends BaseModel {
 
   @column.dateTime({ columnName: "updated_at", autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime;
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
 }
