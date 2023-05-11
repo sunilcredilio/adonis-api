61961e920ff235dc1af93bad50babbbad967adb2
sunil samal <sunil.kumar.23.2@mountblue.tech>
2023-05-11 20:26:00 +0530

    Handled undefined field return by the validator for only space containing fields

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,3 +1,4 @@
+import UndefinedFieldError from "App/Exceptions/CustomErrors/UndefiendFieldError";
 import User from "App/Models/User";
 import UserRegisterValidator from "App/Validators/UserRegisterValidator";
 
@@ -6,12 +7,19 @@ export default class AuthController {
   public async register({ request, response }) {
     try {
       let { email, password } = await request.validate(UserRegisterValidator);
-      email = email.toLowerCase();
-      const savedUser = await User.create({
-        email,
-        password,
-      });
-      response.created(savedUser);
+      
+      //Handling only space contained field,which return undefined after validation
+      if (email === undefined || password === undefined) {
+        const fieldName = !email ? "email" : "password";
+        throw new UndefinedFieldError(`${fieldName} required`, fieldName);
+      } else {
+        email = email.toLowerCase();
+        const savedUser = await User.create({
+          email,
+          password,
+        });
+        response.created(savedUser);
+      }
     } catch (error) {
       throw error;
     }
@@ -20,21 +28,30 @@ export default class AuthController {
   public async login({ request, auth }) {
     try {
       const { email, password } = await request.validate(UserRegisterValidator);
-      const token = await auth
-        .use("api")
-        .attempt(email.toLowerCase(), password, {
-          expiresIn: "10 days",
-        });
-      return token.toJSON();
+      if (email === undefined || password === undefined) {
+        const fieldName = !email ? "email" : "pasword";
+        throw new UndefinedFieldError(`${fieldName} required`, fieldName);
+      } else {
+        const token = await auth
+          .use("api")
+          .attempt(email.toLowerCase(), password, {
+            expiresIn: "10 days",
+          });
+        return token.toJSON();
+      }
     } catch (error) {
       throw error;
     }
   }
   //Revoking the token from api_tokens table
   public async logout({ response, auth }) {
-    await auth.use("api").revoke();
-    response.status(200).json({
-      revoked: true,
-    });
+    try {
+      await auth.use("api").revoke();
+      response.status(200).json({
+        revoked: true,
+      });
+    } catch (error) {
+      throw error;
+    }
   }
 }
