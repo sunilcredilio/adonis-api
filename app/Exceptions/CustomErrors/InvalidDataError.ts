4aeb35fc161b19c513c442c3a56460037471e269
sunilcredilio <sunil.samal@credilio.in>
2023-05-16 17:21:54 +0530

    Removed unwanted exception handling and custom error class

--- app/Exceptions/CustomErrors/InvalidDataError.ts
+++
@@ -1,5 +0,0 @@
-export default class InvalidDataError extends Error {
-    constructor(message: string) {
-      super(message);
-    }
-}
--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -16,10 +16,8 @@
 import Logger from "@ioc:Adonis/Core/Logger";
 import HttpExceptionHandler from "@ioc:Adonis/Core/HttpExceptionHandler";
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-import UndefinedFieldError from "./CustomErrors/UndefiendFieldError";
 import UnauthorizedError from "./CustomErrors/UnauthorizedError";
 import ResourceExistError from "./CustomErrors/ResourceExistError";
-import InvalidDataError from "./CustomErrors/InvalidDataError";
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
@@ -36,20 +34,32 @@ export default class ExceptionHandler extends HttpExceptionHandler {
   //Handling all the errors and send the response based on error
   public async handle( error: any, { response }: HttpContextContract ): Promise<any> {
 
-    if(error.code === "E_VALIDATION_FAILURE"){
+    if (error.code === "E_VALIDATION_FAILURE") {
+      interface Error {
+        field: string;
+        message: string;
+      }
+      // Only returning the field and message to the user 
       const allValidationMessages = error.messages.errors.reduce(
-        ( allValidationErrors: Object[], error: { field: string; message: string }) => {
-          const validationMessage = {};
-          validationMessage["field"] = error.field,
-          validationMessage["message"] = error.message;
-          allValidationErrors.push(validationMessage);
+        (allValidationErrors: Object[], error: Error) => {
+          const errorField = allValidationErrors.filter(
+            (validationError: Error) => {
+              return validationError.field === error.field;
+            }
+          );
+          if (errorField.length === 0) {
+            const validationMessage = {};
+            validationMessage["field"] = error.field,
+            validationMessage["message"] = error.message;
+            allValidationErrors.push(validationMessage);
+          }
           return allValidationErrors;
         },[]);
       response.badRequest({
         messages: allValidationMessages,
         timestamp: new Date(),
       });
-    } else if(error.constraint === "users_email_unique"){
+    } else if (error.constraint === "users_email_unique") {
       response.conflict(this.showMessage("Email already registered"));
     } else if (error.code === "E_INVALID_AUTH_UID") {
       response.unauthorized(this.showMessage("Invalid email address"));
@@ -57,25 +67,13 @@ export default class ExceptionHandler extends HttpExceptionHandler {
       response.unauthorized(this.showMessage("Invalid password"));
     } else if (error.code === "E_ROUTE_NOT_FOUND") {
       response.notFound(this.showMessage("The url you are looking for doesn't exist"));
-    } else if(error.code === 'E_ROW_NOT_FOUND'){
+    } else if (error.code === "E_ROW_NOT_FOUND") {
       response.notFound(this.showMessage("Record not found"));
-    } else if(error.code === "E_UNAUTHORIZED_ACCESS" || error instanceof UnauthorizedError){
+    } else if (error.code === "E_UNAUTHORIZED_ACCESS" ||error instanceof UnauthorizedError) {
       response.unauthorized(this.showMessage("Unauthorized access"));
     } else if (error instanceof ResourceExistError) {
       response.conflict(this.showMessage(error.message));
-    } else if(error instanceof InvalidDataError){
-      response.badRequest(this.showMessage(error.message));
-    } else if(error instanceof UndefinedFieldError){
-      response.badRequest({
-        messages:[
-          {
-            field:`${error.fieldName}`,
-            message:`${error.fieldName} is required`
-          }
-        ],
-        timestamp: new Date()
-      });
-    } else{
+    } else {
       response.internalServerError(this.showMessage("Something went wrong"));
     }
   }
